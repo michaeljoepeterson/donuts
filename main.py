3fd2b254ab64f6bf58d94c61d1814e7176ae8cb8
Michael Peterson <michaeljoepeterson@gmail.com>
2019-03-20 19:57:24 -0600

    modified for_printer soup_print soup and pop functions

--- main.py
+++ main.py
@@ -59,7 +59,34 @@ def send_ticket(data):
     except Exception as ex:
         print(ex)
     time.sleep(1)
+#the functions I added for the for loop again check them my assumption was that after index you only did anything different when m[6] == 1 so then all the other ones could be handled by the same function I also noticed you are basically doing the same thing in each if/else statement so just put it outside the if else and did a check there
+#I also am passing the epson class to the function hoping that the changes it makes will affect the changes to the instance of the class, that might be my JS showing, if it doesn't work like that let me know or modify it
+def big_list_func_1(order_part,printer,num):
+    f = None
+    d = str(drinks[int(order_part[3])]) + '     $1.90 \n'
+    if int(order_part[0]) > 0:
+        f = soup_print(order_part[1])
+    printer.set(font='a', height=3, width=3, align='center')
+    printer.text(str(order_part[5]) + '\n')      
+    printer.set(font='a', height=1, width=1, align='left', text_type='u2')
+    #Use the num passed for the order number instead
+    printer.text('Order ' + str(num) + '\n')
+    printer.set(text_type='normal')
+    #I think this is how single line if statements work in python
+    if f != None: printer.text(f)
+    printer.text(d)
 
+def big_list_func_2(order_part,printer,num):
+    f = None
+    d = 'A ' + str(drinks[int(order_part[3])]) + ' to drink  $1.90 \n'
+    if int(order_part[0]) > 0:
+        f = soup_print(order_part[1])
+
+    printer.set(font='a', height=1, width=1, align='left', text_type='u2')
+    printer.text('Order ' + str(num) + '\n')
+    printer.set(text_type='normal')
+    if f != None: printer.text(f)
+    printer.text(d)
 
 def for_printer(big_list=None):
     if big_list is None:
@@ -69,6 +96,25 @@ def for_printer(big_list=None):
         epson.image("mrsbot.png")
         price = 0.0
         bar = '1234'
+        '''
+        I am guessing it is what tells the printer of the POS what to put on the receipt 
+        You should definitely check these changes since this for loop is more complicated than the other things I changed and I can't run the program locally
+        I am also assuming there are 4 columns? I hope other wise it will run more than 4 times
+        '''
+        for m in big_list:
+            counter = 0
+            if int(m[6]) == 1:
+                big_list_func_1(m,epson,int(m[6]))
+                #placed this here to not mess with later print functions
+                if int(m[0]) > 0:
+                    if m[1] == 1:
+                        price = 4.76
+                    if m[1] == 2:
+                        price = 7.62
+                    bar = m[5]
+            elif int(m[6]) > 1:
+                big_list_func_2(m,epson,int(m[6]))
+        '''
         for m in big_list:
             if int(m[6]) == 1:
                 if int(m[0]) > 0:
@@ -143,6 +189,7 @@ def for_printer(big_list=None):
                     epson.set(text_type='normal')
                     epson.text(d)
                 print(d)
+        '''
         epson.text('Total : ')
         epson.text(str(price) + '\n')
         epson.barcode(bar, 'EAN13', 64, 2, '', '')
@@ -237,6 +284,29 @@ class Pos(Widget):
         self.num[3] = self.key_num
 
     def soup(self, x):
+        #rounded the price for one donut not sure if that will affect any math you have to do, can always change the price back. Cut back on the long if else statement
+        donutStringDict = {
+            "1":{
+                donutText:'1 Bag of donuts',
+                price:4.76
+            },
+            "2":{
+                donutText:'2 Bags of donuts',
+                price:9.52
+            },
+            "3":{
+                donutText:'3 Bags of donuts',
+                price:14.28
+            },
+            "4":{
+                donutText:'4 Bags of donuts',
+                price:19.04
+            }
+        }
+        self.ids.order1S.text = donutStringDict[x].donutText
+        self.ids.order1SZP.text = '$' + str(donutStringDict[x].price)
+        self.cash += donutStringDict[x].price
+        '''
         if x == '1':
             self.ids.order1S.text = '1 Bag of donuts'
             self.ids.order1SZP.text = '$4.76'
@@ -253,7 +323,7 @@ class Pos(Widget):
             self.ids.order1S.text = '4 Bags of donuts'
             self.ids.order1SZP.text = '$19.04'
             self.cash += 19.04
-
+        '''
         self.num[0] = int(x)
         self.ids.soup4.pos = 5500, 470
         self.ids.soup1.pos = 5500, 320
@@ -282,6 +352,34 @@ class Pos(Widget):
         pass
 
     def pop(self, x):
+        #not exactly sure what drink num is but captured it here could further optimize by changing price to variable that is = to 1.90. I put the price here just in case you ever need different prices for the pop
+        popStringDict = {
+            "1":{
+                popText:'Pepsi',
+                price:1.90,
+                drinkNum:1
+            },
+            "2":{
+                popText:'7up',
+                price:1.90,
+                drinkNum:10
+            },
+            "3":{
+                popText:'Root Beer',
+                price:1.90,
+                drinkNum:100
+            },
+            "4":{
+                popText:'Mountain Dew',
+                price:1.90,
+                drinkNum:1000
+            }
+        }
+        self.pop_name[self.pop_index] = popStringDict[x].popText
+        self.pop_name[self.pop_index + 4] = '$' + str(popStringDict[x].price)
+        self.drink_num += popStringDict[x].drinkNum
+
+        '''
         if x == '1':
             self.pop_name[self.pop_index] = 'Pepsi'
             self.pop_name[self.pop_index + 4] = '$1.90'
@@ -298,6 +396,7 @@ class Pos(Widget):
             self.pop_name[self.pop_index] = 'Mountain Dew'
             self.pop_name[self.pop_index + 4] = '$1.90'
             self.drink_num += 1000
+        '''
         self.ids.pops1.text = self.pop_name[0]
         self.ids.pop_price1.text = self.pop_name[4]
         self.ids.pops2.text = self.pop_name[1]
